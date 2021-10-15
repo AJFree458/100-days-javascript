2569787f0a1c54676b8bd7e05edbea2832af4b32
AJFree458 <rthr.frmn@gmail.com>
2021-10-14 22:04:52 -0400

    Create submenu for mobile menu from prev project

---
+++ mobile-submenu/script.js
@@ -0,0 +1,52 @@
+const links = document.querySelectorAll('.nav-list li a');
+// RESPONSIVE MOBILE MENU
+const menuWrapper = document.querySelector('.nav-wrapper');
+const menu = document.querySelector('.nav-list');
+const hamburger = document.querySelector('.hamburger');
+const closeMenu = document.querySelector('.close');
+const subMenuLink = document.querySelector('.submenu-link');
+const backLink = document.querySelector('.back-to-menu');
+const subMenu = document.querySelector('.submenu');
+
+for (const link of links) {
+  link.addEventListener('click', smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute('href');
+  document.querySelector(href).scrollIntoView({
+    behavior: 'smooth',
+  });
+  hideMenu();
+}
+
+const showMenu = () => {
+  hamburger.style.display = 'none';
+  closeMenu.style.transform = 'translateY(0)';
+  menuWrapper.style.transform = 'translateX(0)';
+  menu.style.transform = 'translateX(0)';
+};
+
+const hideMenu = () => {
+  closeMenu.style.transform = 'translateY(-20rem)';
+  hamburger.style.display = 'block';
+  menuWrapper.style.transform = 'translateX(-200%)';
+  menu.style.transform = 'translateX(200%)';
+  subMenu.style.transform = 'translateX(-100%)';
+};
+
+hamburger.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
+menuWrapper.addEventListener('click', hideMenu);
+
+// Submenu Section
+subMenuLink.addEventListener('click', () => {
+  menu.style.transform = 'translateX(-100%)';
+  subMenu.style.transform = 'translateX(0)';
+});
+
+backLink.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0)';
+  subMenu.style.transform = 'translateX(-100%)';
+});
